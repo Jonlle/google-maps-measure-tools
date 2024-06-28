c315a90a989431a77dda11581a94f2d7768b8dcb
Jonlle <jonlle19@gmail.com>
2024-06-27 23:14:19 -0500

    refactor: mejorar el formateo y la gestión del estado en el componente Result
    - Añadir useEffect para actualizar los valores formateados basados en cambios de props
    - Introducir funciones de formateo para el radio, área y perímetro
    - Asegurar que los valores formateados se actualicen solo cuando las props correspondientes cambien

--- src/components/Result.tsx
+++ src/components/Result.tsx
@@ -1,28 +1,47 @@
-// src/components/Result.tsx
 interface ResultProps {
-  area: string | null;
-  perimeter: string | null;
-  radius: string | null;
+  area: number | null;
+  perimeter: number | null;
+  radius: number | null;
 }
 
-const Result = ({ area, perimeter, radius }: ResultProps) => {
+const Result = ({ radius, area, perimeter }: ResultProps) => {
+  const formatRadius = (radius: number) => {
+    const radiusInKilometers = (radius / 1000).toFixed(2);
+    return `${radius.toFixed(0)} m | ${radiusInKilometers} km`;
+  };
+
+  const formatArea = (area: number) => {
+    const areaInSquareKilometers = (area / 1e6).toFixed(2);
+    return `${area.toFixed(0)} m² | ${areaInSquareKilometers} km²`;
+  };
+
+  const formatPerimeter = (perimeter: number) => {
+    const perimeterInKilometers = (perimeter / 1000).toFixed(2);
+    return `${perimeter.toFixed(0)} m | ${perimeterInKilometers} km`;
+  };
+
   return (
     <div className="mt-4 p-4 bg-gray-100 rounded shadow-lg">
-      {radius && (
+      {radius ? (
         <p className="mb-2">
-          <span className="font-semibold">Radio:</span> {radius}
+          <span className="font-semibold">Radio:</span> {formatRadius(radius)}
         </p>
-      )}
-      {area && (
+      ) : null}
+      {area ? (
         <p className="mb-2">
-          <span className="font-semibold">Área:</span> {area}
+          <span className="font-semibold">Área:</span> {formatArea(area)}
         </p>
-      )}
-      {!area && !perimeter && !radius && (
+      ) : null}
+      {perimeter ? (
+        <p className="mb-2">
+          <span className="font-semibold">Perímetro:</span> {formatPerimeter(perimeter)}
+        </p>
+      ) : null}
+      {!area && !perimeter && !radius ? (
         <div className="text-gray-600">
           <p>No hay datos de medición disponibles.</p>
         </div>
-      )}
+      ) : null}
     </div>
   );
 };
